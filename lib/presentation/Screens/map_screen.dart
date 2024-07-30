497568b91f95cda17d27385d23abd2b4b19e36b4
Abdullah Debo <54190547+abdullahdebo@users.noreply.github.com>
2024-07-30 16:43:00 +0300

    Implement Search Bar & Drawer

--- lib/presentation/Screens/map_screen.dart
+++ lib/presentation/Screens/map_screen.dart
@@ -1,15 +1,18 @@
 // ignore_for_file: avoid_unnecessary_containers, prefer_final_fields, unused_import, non_constant_identifier_names
 
 import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_maps/business_logic/cubit/phone_auth/phone_auth_cubit.dart';
 import 'package:flutter_maps/constants/my_colors.dart';
 import 'package:flutter_maps/constants/strings.dart';
 import 'package:flutter_maps/helpers/location_helper.dart';
+import 'package:flutter_maps/presentation/widgets/my_drawer.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:material_floating_search_bar_2/material_floating_search_bar_2.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -20,6 +23,8 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   PhoneAuthCubit phoneAuthCubit = PhoneAuthCubit();
+
+  FloatingSearchBarController controller = FloatingSearchBarController();
 
   static Position? position;
   Completer<GoogleMapController> _MapController = Completer();
@@ -68,10 +73,62 @@ class _MapScreenState extends State<MapScreen> {
         CameraUpdate.newCameraPosition(_myCurrentLocationCameraPosition));
   }
 
+  Widget buildFloatingSearchBar() {
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+
+    return FloatingSearchBar(
+        controller: controller,
+        elevation: 6,
+        hintStyle: GoogleFonts.roboto(fontSize: 18),
+        queryStyle: GoogleFonts.roboto(fontSize: 18),
+        hint: 'Find a place ...',
+        border: BorderSide(style: BorderStyle.none),
+        margins: EdgeInsets.fromLTRB(20, 70, 20, 0),
+        padding: EdgeInsets.fromLTRB(2, 0, 2, 0),
+        height: 52,
+        iconColor: MyColors.blue,
+        scrollPadding: EdgeInsets.only(top: 16, bottom: 56),
+        transitionDuration: Duration(milliseconds: 600),
+        transitionCurve: Curves.easeInOut,
+        physics: BouncingScrollPhysics(),
+        axisAlignment: isPortrait ? 0.0 : -1.0,
+        openAxisAlignment: 0.0,
+        width: isPortrait ? 600 : 500,
+        debounceDelay: Duration(milliseconds: 500),
+        onQueryChanged: (query) {},
+        onFocusChanged: (_) {},
+        transition: CircularFloatingSearchBarTransition(),
+        actions: [
+          FloatingSearchBarAction(
+            showIfOpened: false,
+            child: CircularButton(
+              icon: Icon(
+                Icons.place,
+                color: Colors.black.withOpacity(0.6),
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
+        builder: (context, transition) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: MyDrawer(),
       body: Stack(
+        fit: StackFit.expand,
         children: [
           position != null
               ? buildMap()
@@ -81,7 +138,8 @@ class _MapScreenState extends State<MapScreen> {
                       color: MyColors.blue,
                     ),
                   ),
-                )
+                ),
+          buildFloatingSearchBar(),
         ],
       ),
       floatingActionButton: Container(
---
+++ lib/presentation/widgets/my_drawer.dart
@@ -0,0 +1,189 @@
+// ignore_for_file: sized_box_for_whitespace, must_be_immutable, avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_maps/business_logic/cubit/phone_auth/phone_auth_cubit.dart';
+import 'package:flutter_maps/constants/my_colors.dart';
+import 'package:flutter_maps/constants/strings.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class MyDrawer extends StatelessWidget {
+  MyDrawer({super.key});
+
+  PhoneAuthCubit phoneAuthCubit = PhoneAuthCubit();
+
+  Widget buildDrawerHeader(context) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsetsDirectional.fromSTEB(70, 10, 70, 10),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.blue[100],
+          ),
+          child: Image.asset(
+            'assets/images/debo.jpg',
+            fit: BoxFit.cover,
+          ),
+        ),
+        Text(
+          'Abdullah Debo',
+          style: GoogleFonts.roboto(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(
+          '0534309491',
+          style: GoogleFonts.roboto(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildDrawerListItem(
+      {required IconData leadingIcon,
+      required String title,
+      Widget? trailing,
+      Function()? onTap,
+      Color? color}) {
+    return ListTile(
+      leading: Icon(leadingIcon, color: color ?? MyColors.blue),
+      title: Text(title),
+      trailing: trailing ??
+          Icon(
+            Icons.arrow_right,
+            color: MyColors.blue,
+          ),
+      onTap: onTap,
+    );
+  }
+
+  Widget buildDrawerListItemsDivider() {
+    return Divider(
+      height: 0.0,
+      thickness: 1,
+      indent: 18,
+      endIndent: 24,
+    );
+  }
+
+  Future<void> _launchUrl(String url) async {
+    if (!await launchUrl(url as Uri)) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
+  Widget buildIcon(IconData icon, String url) {
+    return InkWell(
+      onTap: () => _launchUrl(url),
+      child: Icon(
+        icon,
+        color: MyColors.blue,
+        size: 35,
+      ),
+    );
+  }
+
+  Widget buildSocialMediaIcons() {
+    return Padding(
+      padding: const EdgeInsetsDirectional.only(
+        start: 16,
+      ),
+      child: Row(
+        children: [
+          buildIcon(
+            FontAwesomeIcons.twitter,
+            'https://x.com/valencawie',
+          ),
+          const SizedBox(
+            width: 15,
+          ),
+          buildIcon(
+            FontAwesomeIcons.instagram,
+            'https://www.instagram.com/deboabdullah?igsh=MWY2ZGE2ZHc3MnBjMg%3D%3D&utm_source=qr',
+          ),
+          const SizedBox(
+            width: 15,
+          ),
+          buildIcon(
+            FontAwesomeIcons.linkedinIn,
+            'www.linkedin.com/in/abdullah-debo-2374a317b',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildLogOutBlocProvider(context) {
+    return Container(
+      child: BlocProvider<PhoneAuthCubit>(
+        create: (context) => phoneAuthCubit,
+        child: buildDrawerListItem(
+          leadingIcon: Icons.logout,
+          title: 'LogOut',
+          onTap: () async {
+            await phoneAuthCubit.logOut();
+            Navigator.of(context).pushReplacementNamed(loginScreen);
+          },
+          color: Colors.red,
+          trailing: SizedBox(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Container(
+            height: 315,
+            child: DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+              ),
+              child: buildDrawerHeader(context),
+            ),
+          ),
+          buildDrawerListItem(leadingIcon: Icons.person, title: 'My Profile'),
+          buildDrawerListItemsDivider(),
+          buildDrawerListItem(
+            leadingIcon: Icons.history,
+            title: 'Places History',
+            onTap: () {},
+          ),
+          buildDrawerListItemsDivider(),
+          buildDrawerListItem(leadingIcon: Icons.settings, title: 'Settings'),
+          buildDrawerListItemsDivider(),
+          buildDrawerListItem(leadingIcon: Icons.help, title: 'Help'),
+          buildDrawerListItemsDivider(),
+          buildLogOutBlocProvider(context),
+          const SizedBox(
+            height: 235,
+          ),
+          ListTile(
+            leading: Text(
+              'Follow us',
+              style: GoogleFonts.roboto(
+                color: Colors.grey[600],
+                fontSize: 18,
+              ),
+            ),
+          ),
+          buildSocialMediaIcons(),
+        ],
+      ),
+    );
+  }
+}
